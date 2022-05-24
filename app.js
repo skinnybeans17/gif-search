299adf961bd5281e6cbca242a5d12ea5b4132edc
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-05-24 14:35:23 -0700

    s

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 // Middleware
 const exphbs  = require('express-handlebars');
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Routes
@@ -16,6 +16,13 @@ app.get('/', (req, res) => {
   const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
   // render the hello-gif view, passing the gifUrl into the view to be displayed
   res.render('hello-gif', { gifUrl })
+})
+
+app.get('/greetings/:name', (req, res) => {
+  // grab the name from the path provided
+  const name = req.params.name;
+  // render the greetings view, passing along the name
+  res.render('greetings', { name });
 })
 
 // Start Server
